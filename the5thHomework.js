b17e3acd18707d1810fa4784fccf842ac0ebdf42
Artsiom <43150676+ra44o@users.noreply.github.com>
2018-11-02 18:23:05 +0300

    add file of my homework
    trying to replicate operators "new" and "instanceof"

---
+++ the5thHomework.js
@@ -0,0 +1,45 @@
+// 1. Write the "new" operator as if it was implemented in JS.
+function NEW(constructor, args) {
+    let n = {'__proto__': constructor.prototype};
+    constructor.apply(n, args);
+    return n;
+
+    // OR
+    // return new constructor(...args);
+}
+
+function Person (name) {
+	this.name = name;
+}
+
+Person.prototype.speak = function() { 
+	console.log('Hello! ' + this.name);
+}
+
+var person = NEW(Person, ['Gena']);
+
+person.speak(); // --> 'Hello';
+
+
+
+// 2. Write the "instanceof" operator as if it was implemented in JS.
+var Person = function(name) {
+	this.name = name;
+}
+
+var Human = function(name) {
+    this.name = name;
+}
+
+Person.prototype = new Human();
+
+function INSTANCEOF(object, constructor) {
+    if (object.__proto__ === constructor.prototype) {
+        return true;
+    } else if (object.__proto__ !== null) { //для выхода из рекурсии если прошли __proto__ самого последнего родителя
+        return INSTANCEOF(object.__proto__, constructor);
+    } else return false;
+}
+
+person = new Person( 'Alexis' );
+console.log (INSTANCEOF( person, Human )); // --> true
