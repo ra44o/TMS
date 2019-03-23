ba4bc4b1de31a2887fd8fae242d9ffc8a90d0eb1
Artsiom Rak <ra44elo@gmail.com>
2019-03-23 18:22:57 +0300

    feat: add calculateWay method

--- js-classes/inheritance-es5.js
+++ js-classes/inheritance-es5.js
@@ -8,19 +8,30 @@ function Car(name, model, year, color, maxSpeed, fuelCapacity = 60, fuelConsumpt
   this.fuelConsumption = fuelConsumption;
 }
 
-Car.prototype.getFullName = function () {
-  return this.name;
+Car.prototype.getFullName = function() {
+  return `${this.name} ${this.model}`;
 }
 
-Car.prototype.getAge = function () {
+Car.prototype.getAge = function() {
   return new Date() - this.year;
 }
 
-Car.prototype.changeColor = function (newColor) {
+Car.prototype.changeColor = function(newColor) {
   if (newColor === this.color) {
     console.log('Your car is in this color already.');
   } else {
     this.color = newColor;
-    console.log('Color changed on ' + this.color);
+    console.log(`Color changed on ${this.color}.`);
+  }
+}
+
+Car.prototype.calculateWay = function(kilometres, fuel) {
+  if (fuel < 10) {
+    console.log(`You'd better to refuel because you have less than 10 litres of fuel.`);
+  }
+  const wayTime = ((kilometres / (this.maxSpeed * 0.5))*60).toFixed(0);
+  console.log(`You need ${wayTime} minutes to reach your destination.`);
+  if (this.fuelCapacity - kilometres / 100 * this.fuelConsumption < 0) {
+    console.log(`You will need to refuel ${Math.ceil(kilometres / 100 * this.fuelConsumption / this.fuelCapacity)} times.`);
   }
 }
